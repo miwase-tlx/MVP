2310e8828f54847928c7f14bfed07a5535c0e62a
miwase-tlx <175207471+miwase-tlx@users.noreply.github.com>
2024-10-29 10:46:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -25,7 +25,8 @@ function App() {
         <ThalexMVPCalculator />
       </ErrorBoundary>
       <p>
-      This dashboard shows indicative volume for each product, for relevant trading days start and stop at 8am UTC. Note that the volumes shown on this page can lag behind actual trading volume, and are for indicative purposes only.
+      This dashboard shows indicative volume for each product, for relevant trading days start and stop at 8am UTC. 
+      Note that the volumes shown on this page can lag behind actual trading volume, and are for indicative purposes only.
       </p>
     </div>
   );
